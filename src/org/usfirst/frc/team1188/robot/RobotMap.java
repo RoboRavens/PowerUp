d792f1c67c2ea88e9f6e05f0160f1fc072ce8d1e
Alex Hoyer <tautologicaloxymoron14@gmail.com>
2018-02-17 14:41:08 -0500

    Organize robot map

--- src/org/usfirst/frc/team1188/robot/RobotMap.java
+++ src/org/usfirst/frc/team1188/robot/RobotMap.java
@@ -11,25 +11,27 @@ public class RobotMap {
 	public static final int rightDriveEncoder1 = 2;
 	public static final int rightDriveEncoder2 = 3;
 	
-	
-	public static final int elevatorEncoder1 = 20;
-	public static final int elevatorEncoder2 = 21;
-	
 	public static final int shiftToLowGearSolenoid = 0;
 	public static final int shiftToHighGearSolenoid = 1;
+	
+	// Elevator System
+	public static final int elevatorEncoder1 = 20;
+	public static final int elevatorEncoder2 = 21;
 	
 	public static final int elevatorMotorLeft = 3;
 	public static final int elevatorMotorRight = 2;
 	
 	
-	//Intake system *Update values!*
+	// Intake System 
 	public static final int intakeMotorLeft = 0;
 	public static final int intakeMotorRight = 1;
 	public static final int leftIntakeClampSolenoid = 2;
 	public static final int rightIntakeClampSolenoid = 3;
+
+	// Arm System
+	public static final int armMotorLeft = 5;
+	public static final int armMotorRight = 4;
 	
 	public static final int carriageStalledLightRelay = 1;
 	
-	public static final int armMotorLeft = 5;
-	public static final int armMotorRight = 4;
 }
