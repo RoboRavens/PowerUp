faa58363dc8edba4553e81cd5444ee7d9fd5bb96
Nick Johnson <njemail25@gmail.com>
2018-02-23 21:07:18 -0500

    add several autonomous commands

---
+++ src/org/usfirst/frc/team1188/robot/AutonomousCalibrations.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team1188.robot;
+
+public class AutonomousCalibrations {
+	
+	public static final double AutonomousCrossAutoLineDriveForwardInches = 120;
+	public static final double AutonomousCrossAutoLineDriveForwardPowerMagnitude = .25;
+	
+	public static final double AutonomousScoreLeftSwitchPosition1DriveForwardInches = 140;
+	public static final double AutonomousScoreLeftSwitchPosition3FirstDriveForwardInches = 50;
+	public static final double AutonomousScoreLeftSwitchPosition3SecondDriveForwardInches = 226.5;
+	public static final double AutonomousScoreLeftSwitchPosition3ThirdDriveForwardInches = 118;
+	public static final double AutonomousScoreLeftSwitchDriveForwardPowerMagnitude = .25;
+	
+	public static final double AutonomousScoreLeftSwitchGrabCubePosition1FirstDriveForwardInches = 118;
+	public static final double AutonomousScoreLeftSwitchGrabCubePosition1SecondDriveForwardInches = 132;
+
+
+	public static final double AutonomousScoreSwitchIntakePushPowerMagnitude = .1;
+	public static final double AutonomousGrabCubeIntakePullPowerMagnitude = 1;
+	
+	public static final double AutonomousArmExtensionPowerMagnitude = .3;
+	public static final double AutonomousArmExtensionEncoderDistance = 2048;
+}
