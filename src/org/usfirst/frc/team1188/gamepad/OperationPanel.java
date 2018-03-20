781a33e6193773ea763222fdee6bb836b5eb01f4
Alexander Hoyer <tautologicaloxymoron14@gmail.com>
2018-03-20 19:39:43 -0400

    update button numbers for operation panel

--- src/org/usfirst/frc/team1188/gamepad/OperationPanel.java
+++ src/org/usfirst/frc/team1188/gamepad/OperationPanel.java
@@ -1,7 +1,5 @@
 package org.usfirst.frc.team1188.gamepad;
 
-import org.usfirst.frc.team1188.robot.Calibrations;
-
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
@@ -20,64 +18,42 @@ public class OperationPanel {
 		return new JoystickButton(joystick, getButtonNumber(button));
 	}
 	
-	public boolean getSwitchIsFlipped(AxisCode axis) {
-		boolean isFlipped = false;
-		double axisValue = this.getAxis(axis);
-		
-		if (axisValue >= Calibrations.AXIS_IS_PRESSED_VALUE) {
-			isFlipped = true;
-		}
-		
-		return isFlipped;
-	}
-	
-	public double getAxis(AxisCode axis) {
-		double axisValue;
-		// TODO: Fix and find values for switches
-		switch (axis) {
-			case ARMDOUBLEOVERRIDEDOWN:
-				axisValue = joystick.getRawAxis(0);
-				break;
-			case ARMDOUBLEOVERRIDEUP:
-				axisValue = joystick.getRawAxis(1);
-				break;
-			case ELEVATORDOUBLEOVERRIDEDOWN:
-				axisValue = joystick.getRawAxis(2);
-				break;
-			case ELEVATORDOUBLEOVERRIDEUP:
-				axisValue = joystick.getRawAxis(3);
-				break;
-			default:
-				axisValue = 0;
-				break;
-		}
-		return axisValue;
-	}
-	
 	public int getButtonNumber(ButtonCode button) {
 		int buttonNumber;
-		// TODO:change button number values
+		
 		switch (button) {
-		case ARMMANUALOVERRIDEUP:
-			buttonNumber = 5;
+		case ELEVATORDOUBLEOVERRIDEDOWN:
+			buttonNumber = 1;
 			break;
-		case ARMMANUALOVERRIDEDOWN:
-			buttonNumber = 6;
+		case ELEVATORDOUBLEOVERRIDEUP:
+			buttonNumber = 2;
 			break;
-		case ELEVATORMANUALOVERRIDEUP:
-			buttonNumber = 9;
+		case ARMDOUBLEOVERRIDEDOWN:
+			buttonNumber = 3;
+			break;
+		case ARMDOUBLEOVERRIDEUP:
+			buttonNumber = 4;
 			break;
 		case ELEVATORMANUALOVERRIDEDOWN:
-			buttonNumber = 10;
+			buttonNumber = 5;
+			break;
+		case ELEVATORMANUALOVERRIDEUP:
+			buttonNumber = 6;
+			break;
+		case ARMMANUALOVERRIDEDOWN:
+			buttonNumber = 7;
+			break;
+		case ARMMANUALOVERRIDEUP:
+			buttonNumber = 8;
 			break;
 		case ELEVATOREXTEND:
-			buttonNumber = 11;
+			buttonNumber = 9;
 			break;
 		case ELEVATORMIDRANGE:
-			buttonNumber = 12;
+			buttonNumber = 10;
 			break;
 		case ELEVATORRETRACT:
-			buttonNumber = 13;
+			buttonNumber = 11;
 			break;
 		default:
 			throw new IllegalArgumentException("Invalid Button Code");
