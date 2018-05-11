457eabec814690f84444dd0c6e5d3abe6a11f390
Matthew Orlando <09orlandom@gmail.com>
2018-05-10 21:14:22 -0400

    update to use GradleRIO

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 [PC and Robot Setup](docs/setup.md)  
 [Code Standards](docs/code-standards.md)  
 [Shuffleboard](shuffleboard/README.md)
+
+### GradleRIO
+After cloning the repo open a powershell window in the root of the repository and run `./gradlew build` followed by `./gradlew eclipse`. If eclipse was already open make sure to refresh the project through the eclipse interface.
--- testsrc/tests/subsystems/ElevatorSubsystemTests.java
+++ testsrc/tests/subsystems/ElevatorSubsystemTests.java
@@ -1,12 +1,16 @@
 package tests.subsystems;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.usfirst.frc.team1188.ravenhardware.IBufferedDigitalInput;
+import org.usfirst.frc.team1188.robot.subsystems.ElevatorSubsystem2;
+import org.usfirst.frc.team1188.wpiwrappers.IEncoder;
+
+import com.ctre.phoenix.motorcontrol.IMotorController;
 
 // http://www.baeldung.com/mockito-junit-5-extension
 
@@ -15,8 +19,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class ElevatorSubsystemTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test(@Mock IMotorController elevatorMotor, @Mock IEncoder encoder, @Mock IBufferedDigitalInput bottomLimitSwitch, @Mock IBufferedDigitalInput topLimitSwitch) {
+		//TODO create wrapper for wpi Timer
+		ElevatorSubsystem2 sub = new ElevatorSubsystem2(elevatorMotor, encoder, bottomLimitSwitch, topLimitSwitch);
 	}
 
 }
